e91238d2131cf9db1a42fbf46beb2e5c07f9ac84
AthulMckell <51241239+AthulMckell@users.noreply.github.com>
2022-01-10 10:06:17 +0530

    changes

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/',(req,res)=>
 res.send('WELCOME, Athul RAJ fsd b1')
 );
 
+app.get('/home',(req,res)=>
+res.sendFile(__dirname + "/views/index.html"));
+
+app.post("/home", (req, res) => {
+    res.send("Thank you for subscribing")});
+
+// app.post('/mailer', (req,res))
+
 
 app.listen(PORT, () =>{
     console.log(`Your server is running on port ${PORT}`);
