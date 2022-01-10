c2cd8e80156dd73051bfcf1ebffea0a2e4130c27
AthulMckell <51241239+AthulMckell@users.noreply.github.com>
2022-01-10 12:00:48 +0530

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-Parser');
 
 const PORT = 5000;
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/',(req,res)=>
 res.send('WELCOME, Athul RAJ fsd b1')
@@ -13,7 +16,11 @@ res.sendFile(__dirname + "/views/index.html"));
 app.post("/home", (req, res) => {
     res.send("Thank you for subscribing")});
 
-// app.post('/mailer', (req,res))
+app.post('/mailer', (req,res)=>
+{   
+    console.log(req.body.mail);
+    res.send(`Email ${req.body.mail}`);
+});
 
 
 app.listen(PORT, () =>{
